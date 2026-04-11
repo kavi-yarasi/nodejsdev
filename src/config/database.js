39a103c74ef5c2c3de4bc56efb349f1a05ff903e
Kaviyarasi <Kaviyarasi.Karuppuchamy@corestack.io>
2026-04-11 08:07:48 +0530

    email validation using validator npm package

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://lkaviyarasik4_db_user:14jiS1GZCEgOAAyd@kaviscluster.uf2jwci.mongodb.net/devTinder?appName=kaviscluster");
+
     //database and cluster name with string
 }
 
